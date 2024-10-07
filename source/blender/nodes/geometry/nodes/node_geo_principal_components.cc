e05fd2e4b69ca6b71c31bab1864dda3eb8312553
ComicAddict <tolgatalhayldz@gmail.com>
2024-10-06 22:30:38 -0500

    updating function call for regitering

--- source/blender/nodes/geometry/nodes/node_geo_principal_components.cc
+++ source/blender/nodes/geometry/nodes/node_geo_principal_components.cc
@@ -220,7 +220,7 @@ static void node_register()
   ntype.draw_buttons = node_layout;
   ntype.declare = node_declare;
   ntype.gather_link_search_ops = node_gather_link_searches;
-  blender::bke::nodeRegisterType(&ntype);
+  blender::bke::node_register_type(&ntype);
 
   node_rna(ntype.rna_ext.srna);
 }
